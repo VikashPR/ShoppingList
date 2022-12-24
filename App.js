953307928f34f78093fec7dd2b6d663e5d587c27
Vikash2806 <vikashrajendran2806@gmail.com>
2022-12-24 19:32:45 +0530

    feat: delete items add

--- App.js
+++ App.js
@@ -3,28 +3,45 @@ import {View, Text, FlatList, StyleSheet} from 'react-native';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
 import uuid from 'react-native-uuid';
-const items = [
-  {
-    id: uuid.v4(),
-    text: 'Cat',
-  },
-  {
-    id: uuid.v4(),
-    text: 'Fish',
-  },
-  {
-    id: uuid.v4(),
-    text: 'Meat',
-  },
-];
+
 
 const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuid.v4(),
+      text: 'Cat',
+    },
+    {
+      id: uuid.v4(),
+      text: 'Fish',
+    },
+    {
+      id: uuid.v4(),
+      text: 'Meat',
+    },
+    {
+      id: uuid.v4(),
+      text: 'Taste',
+    },
+    {
+      id: uuid.v4(),
+      text: 'Good',
+    },
+  ]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
   return (
     <View style={styles.container}>
       <Header title="Shopping List" />
       <FlatList
         data={items}
-        renderItem={({item}) => <ListItem item={item} />}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
         keyExtractor={item => item.id}
       />
     </View>
--- components/ListItem.js
+++ components/ListItem.js
@@ -2,12 +2,17 @@ import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const ListItem = ({item}) => {
+const ListItem = ({item, deleteItem}) => {
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
         <Text style={styles.listItemText}>{item.text}</Text>
-        <Icon name="remove" size={30} color="#900" />
+        <Icon
+          name="remove"
+          size={30}
+          color="firebrick"
+          onPress={() => deleteItem(item.id)}
+        />
       </View>
     </TouchableOpacity>
   );
